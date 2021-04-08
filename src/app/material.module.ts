490bdcc9ffbbc0b53c66bffc90b8d0a906dafd15
roberto.rosa7@gmail.com <roberto.rosa7@gmail.com>
2021-04-08 19:27:20 -0300

    remove ngx electron

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatButtonModule } from '@angular/material/button'
-import { MatDialogModule } from '@angular/material/dialog'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
-import { HttpClientModule } from '@angular/common/http'
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MatCardModule } from '@angular/material/card'
-import { CurrencyMaskModule } from 'ng2-currency-mask'
-import { MatSnackBarModule } from '@angular/material/snack-bar'
-import { MatSortModule } from '@angular/material/sort'
-import { MatTableModule } from '@angular/material/table'
-import { MatDatepickerModule } from '@angular/material/datepicker'
-import { MatNativeDateModule } from '@angular/material/core'
-import { MomentDateModule } from '@angular/material-moment-adapter'
-import { MatListModule } from '@angular/material/list'
-import { MatMenuModule } from '@angular/material/menu'
-import { MatSelectModule } from '@angular/material/select'
-import { MatAutocompleteModule } from '@angular/material/autocomplete'
-import { MatExpansionModule } from '@angular/material/expansion'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
-import { MatTabsModule } from '@angular/material/tabs'
-=======
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -58,8 +27,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
-import { NgxElectronModule } from 'ngx-electron';
->>>>>>> d7cae6ef32573f3d5bbf7ce2f09a97ec584543d7
 
 @NgModule({
   declarations: [],
